3221d326f786706fa5b77c0145398341c1157c77
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-02-22 20:45:56 -0500

    TestPersistence

--- TreePLE-Spring/src/test/java/ca/mcgill/ecse321/TreePLE/persistence/TestPersistence.java
+++ TreePLE-Spring/src/test/java/ca/mcgill/ecse321/TreePLE/persistence/TestPersistence.java
@@ -10,6 +10,14 @@ import ca.mcgill.ecse321.TreePLE.model.Municipality;
 import ca.mcgill.ecse321.TreePLE.model.Tree;
 import ca.mcgill.ecse321.TreePLE.model.TreeManager;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Calendar;
+import java.io.File;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
 
 public class TestPersistence {
 
@@ -18,18 +26,56 @@ public class TestPersistence {
 	@Before
 	public void setUp() throws Exception {
 		tm = new TreeManager();
-		Municipality municipality = new Municipality("lel");
+		
 		// create trees
-		Tree tree1 = new Tree(12, 23, 34, 34, municipality);
+		Municipality municipality1 = new Municipality("Westmount");
+		Municipality municipality2 = new Municipality("Outremont");
+		
+		
+		Tree tree1 = new Tree(12, 23, 34, 34, municipality1);
+		Tree tree2 = new Tree(34, 24, 26, 34, municipality2);
+		
+		// manage trees
+		tm.addTree(tree1);
+		tm.addTree(tree2);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		tm.delete();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+		// initialize model file
+	    PersistenceXStream.initializeModelManager("output"+File.separator+"data.xml");
+	    // save model that is loaded during test setup
+	    if (!PersistenceXStream.saveToXMLwithXStream(tm))
+	        fail("Could not save file.");
 
+	    // clear the model in memory
+	    tm.delete();
+	    assertEquals(0, tm.getTrees().size());
+
+	    // load model
+	    tm = (TreeManager) PersistenceXStream.loadFromXMLwithXStream();
+	    if (tm == null)
+	        fail("Could not load file.");
+
+	    // check event
+	    /**assertEquals(1, tm.getEvents().size());
+	    assertEquals("Concert", tm.getEvent(0).getName());
+	    Calendar c = Calendar.getInstance();
+	    c.set(2015,Calendar.SEPTEMBER,15,8,30,0);
+	    Date eventDate = new Date(c.getTimeInMillis());
+	    Time startTime = new Time(c.getTimeInMillis());
+	    c.set(2015,Calendar.SEPTEMBER,15,10,0,0);
+	    Time endTime = new Time(c.getTimeInMillis());
+	    assertEquals(eventDate.toString(), tm.getEvent(0).getEventDate().toString());
+	    assertEquals(startTime.toString(), tm.getEvent(0).getStartTime().toString());
+	    assertEquals(endTime.toString(), tm.getEvent(0).getEndTime().toString());
+	    */
+	}
 }
+
+
