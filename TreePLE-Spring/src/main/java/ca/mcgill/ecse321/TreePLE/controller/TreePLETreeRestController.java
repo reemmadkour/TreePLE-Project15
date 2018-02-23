79f9c25bdc18d637c348b504377e37e2c9db30e0
reemmadkour <reem.madkour@mail.mcgill.ca>
2018-02-23 18:12:38 -0500

    controller first method

--- TreePLE-Spring/src/main/java/ca/mcgill/ecse321/TreePLE/controller/TreePLETreeRestController.java
+++ TreePLE-Spring/src/main/java/ca/mcgill/ecse321/TreePLE/controller/TreePLETreeRestController.java
@@ -2,11 +2,18 @@ package ca.mcgill.ecse321.TreePLE.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.TreePLE.dto.TreeDto;
+import ca.mcgill.ecse321.TreePLE.model.Municipality;
+import ca.mcgill.ecse321.TreePLE.model.Municipality.MunicipalityName;
 import ca.mcgill.ecse321.TreePLE.model.Tree;
+import ca.mcgill.ecse321.TreePLE.model.Tree.LandType;
+import ca.mcgill.ecse321.TreePLE.model.Tree.TreeSpecies;
+import ca.mcgill.ecse321.TreePLE.service.InvalidInputException;
 import ca.mcgill.ecse321.TreePLE.service.TreePLETreeService;
 
 @RestController
@@ -30,19 +37,24 @@ public class TreePLETreeRestController {
 		  return treeDto;
 		}
 	
-	/**@PostMapping(value = { "/PlantTree/{landType}/{species}/{height}/{diameter}/{longitude}/{latidude}/{municipality}/"})
-	public EventDto createEvent(
-			@PathVariable ("landType") String name,
-			@RequestParam Date date,
-			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern="HH:mm") LocalTime startTime,
-			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern="HH:mm") LocalTime endTime
+	@PostMapping(value = { "/PlantTree/{landType}/{species}/{height}/{diameter}/{longitude}/{latitude}/{municipality}/"})
+	public TreeDto plantTree(
+			@PathVariable ("landType") LandType landtype,
+			@PathVariable  ("species") TreeSpecies species,
+			@PathVariable  ("height") double height,
+			@PathVariable  ("longitude") double longitude,
+			@PathVariable  ("latitude") double latitude,
+			@PathVariable  ("diameter") double diameter,
+			@PathVariable  ("municipality") MunicipalityName municipality
+			
 			) throws InvalidInputException {
-		@SuppressWarnings("deprecation")
-		Time startTimeSql = new Time(startTime.getHour(),startTime.getMinute(), 0);
-		@SuppressWarnings("deprecation")
-		Time endTimeSql = new Time(endTime.getHour(),endTime.getMinute(), 0);
-		Event event = service.createEvent(name, date, startTimeSql, endTimeSql);
-		return convertToDto(event);
-}*/
+
+	;
+	Municipality mun= new Municipality();
+	mun.setMunicipalityName(municipality);
+		Tree tree= service.plantTree(landtype, species, height,diameter, longitude, latitude, mun);
+		
+		return convertToDto(tree);
+}
 	
 }
