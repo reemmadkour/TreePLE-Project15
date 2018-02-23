519a4a30acd9cd32929b21e5a7fa9b1b0a4331b0
mahdis-asaadi <mahdis.asaadi@mail.mcgill.ca>
2018-02-22 22:56:16 -0500

    StatusDto

--- TreePLE-Spring/src/main/java/ca/mcgill/ecse321/TreePLE/dto/StatusDto.java
+++ TreePLE-Spring/src/main/java/ca/mcgill/ecse321/TreePLE/dto/StatusDto.java
@@ -1,5 +1,18 @@
 package ca.mcgill.ecse321.TreePLE.dto;
 
+import java.util.*;
+
+import ca.mcgill.ecse321.TreePLE.model.Status.TreeState; 
+
 public class StatusDto {
 
+	private TreeState treeState;
+	
+	public void StatusDto(TreeState treeState) {
+		this.treeState=treeState;
+	}
+	
+	public TreeState getTreeState() {
+		return treeState;
+	}
 }
