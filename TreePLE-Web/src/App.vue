4de6da89265895052776031869a6fc29c0a1a5c1
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-04 14:14:23 -0400

    fv1

--- TreePLE-Web/src/App.vue
+++ TreePLE-Web/src/App.vue
@@ -1,6 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">TreePLE  <img src="./assets/logo.png"></a>
+     
+      <div class="navbar" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/#/municipalities">Municipalities <span class="sr-only">(current)</span></a>
+          </li>
+         
+           <li class="nav-item active">
+            <a class="nav-link" href="/#/map">Map <span class="sr-only">(current)</span></a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link" href="/#/trees">Trees <span class="sr-only">(current)</span></a>
+          </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="/#/residents"> Residents <span class="sr-only">(current)</span></a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link" href="/#/transactions"> Transactions <span class="sr-only">(current)</span></a>
+          </li>
+
+          <li class="nav-item active">
+            <a class="nav-link" href="http://ecse321-12.ece.mcgill.ca:8087/app-release.apk"> Download APK <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <br>
     <router-view></router-view>
   </div>
 </template>
@@ -9,6 +43,7 @@
 export default {
   name: 'app'
 }
+
 </script>
 
 <style>
@@ -20,4 +55,25 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+img {
+  display:inline;
+  width:3em;
+  margin-right:5em;
+}
+
+#navMenu {
+  background-color:black;
+}
+
+#navMenu li {
+  float: left;
+  display: inline;
+  margin:0;
+  width:10%;
+}
+
 </style>
+
+
+
