1900376e0ddbc60e597cd0ebc61ca5ea9fcff022
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-05 16:37:36 -0400

    rm css removed

--- TreePLE-Web/src/components/TreePLE.vue
+++ TreePLE-Web/src/components/TreePLE.vue
@@ -1,7 +1,5 @@
 <template>
-<div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
-  <span class="w3-bar-item w3-right"> TreePLE</span>
-</div>
+
 <div id="TreePLE">
 
     <h1>TreePLE</h1> 
