a87ad166faf7e2dfaef6a89bfccb32db0bb5efaa
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-06 19:30:26 -0400

    nav bar.14

--- TreePLE-Web/src/App.vue
+++ TreePLE-Web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse bg-inverse">
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
       <a class="navbar-brand" href="/">TreePLE  <img src="./assets/logo.jpg"></a>
      
       <div class="navbar" id="navbarSupportedContent">
@@ -21,7 +21,7 @@
           </li>
 
           <li class="nav-item active">
-            <a class="nav-link" href="/#/residents"> List Trees <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/#/app"> List Trees <span class="sr-only">(current)</span></a>
           </li>
 
            <li class="nav-item active">
