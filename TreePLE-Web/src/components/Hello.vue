5cda0aa5d5e6a54142a5055eb5f9ba8d230f2f74
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-07 13:04:20 -0400

    login

--- TreePLE-Web/src/components/Hello.vue
+++ TreePLE-Web/src/components/Hello.vue
@@ -19,12 +19,12 @@
 </div>
 </template>
 <script>
-function myMap() {
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({position:myCenter});
-marker.setMap(map);
+function myMap () {
+  var myCenter = new google.maps.LatLng(41.878114, -87.629798)
+  var mapProp = {center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId:     google.maps.MapTypeId.ROADMAP}
+  var map = new google.maps.Map(document.getElementById('googleMap'), mapProp)
+  var marker = new google.maps.Marker({position: myCenter})
+  marker.setMap(map)
 }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
