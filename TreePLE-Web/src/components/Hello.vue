51814fdaa22dfd7dc71cef7b5512efb0d5e4aded
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-07 13:14:34 -0400

    login

--- TreePLE-Web/src/components/Hello.vue
+++ TreePLE-Web/src/components/Hello.vue
@@ -15,20 +15,9 @@
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
-<div id="googleMap" style="height:400px;width:100%;"></div>
-</div>
-</template>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
 
-<script>
-function myMap () {
-  var myCenter = new google.maps.LatLng(41.878114, -87.629798)
-  var mapProp = {center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP}
-  var map = new google.maps.Map(document.getElementById('googleMap'), mapProp)
-  var marker = new google.maps.Marker({position: myCenter})
-  marker.setMap(map)
-}
-</script>
+</template>
+
 <script>
 export default {
   name: 'hello',
