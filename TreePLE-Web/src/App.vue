52227b349921d440e9559c5858aafbd92e14339b
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-06 20:29:27 -0400

    nav bar.19

--- TreePLE-Web/src/App.vue
+++ TreePLE-Web/src/App.vue
@@ -60,7 +60,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  
   margin-top: 60px;
 }
 
@@ -70,16 +70,6 @@ img {
   margin-right:5em;
 }
 
-#navMenu {
-  background-color:blue;
-}
-
-#navMenu li {
-  float: left;
-  display: inline;
-  margin:0;
-  width:10%;
-}
 
 </style>
 
