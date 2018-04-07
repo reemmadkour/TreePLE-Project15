61d2d688cd8298532ad09c4b57c08c24b2ccf83e
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-06 20:32:41 -0400

    nav bar.19

--- TreePLE-Web/src/App.vue
+++ TreePLE-Web/src/App.vue
@@ -1,25 +1,13 @@
 <template>
   <div id="app">
-	<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
-    <nav class="navbar navbar-expand-lg sticky-top navbar-light" style="background-color: #000033;">
+
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-inverse">
       <a class="navbar-brand" href="/">TreePLE  <img src="./assets/logo.jpg"></a>
      
       <div class="navbar" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home style="color: #ffffff"<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="/#/municipalities"><i class="fa fa-eye fa-fw"></i>  View Trees<span class="sr-only">(current)</span></a>
