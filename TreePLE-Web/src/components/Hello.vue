11809fe04dcea87294e922c791a49865e405f379
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-07 13:11:13 -0400

    login

--- TreePLE-Web/src/components/Hello.vue
+++ TreePLE-Web/src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
       
-	<form action="/app">
+	<form action="/#/app">
   <div class="form-group">
     <label for="email">Email address:</label>
     <input type="email" class="form-control" id="email">
@@ -18,16 +18,17 @@
 <div id="googleMap" style="height:400px;width:100%;"></div>
 </div>
 </template>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+
 <script>
 function myMap () {
   var myCenter = new google.maps.LatLng(41.878114, -87.629798)
-  var mapProp = {center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId:     google.maps.MapTypeId.ROADMAP}
+  var mapProp = {center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP}
   var map = new google.maps.Map(document.getElementById('googleMap'), mapProp)
   var marker = new google.maps.Marker({position: myCenter})
   marker.setMap(map)
 }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
 <script>
 export default {
   name: 'hello',
