e56bf8cddc40e51c68015a5c7ba3bbca128e8a61
abed-atassi <abed.atassi@mail.mcgill.ca>
2018-04-06 23:14:09 -0400

    login

--- TreePLE-Web/src/components/Hello.vue
+++ TreePLE-Web/src/components/Hello.vue
@@ -17,8 +17,7 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
-  </div>
-  <div> 
+  
 	<form action="/action_page.php">
   <div class="form-group">
     <label for="email">Email address:</label>
