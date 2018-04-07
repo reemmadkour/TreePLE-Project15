0eb80cb74bf2deaee0c1f9f9c8a272e2b71614cc
mahdis-asaadi <mahdis.asaadi@mail.mcgill.ca>
2018-04-07 14:01:09 -0400

    Changing to grey

--- TreePLE-Web/src/components/TreePLE.vue
+++ TreePLE-Web/src/components/TreePLE.vue
@@ -6,14 +6,16 @@
     <h2> List of all trees </h2>
 
 <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">List Trees by
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-      <li><a href="#">All</a></li>
-      <li><a href="#">Municipality</a></li>
-      <li><a href="#">Species</a></li>
-    </ul>
+ <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    List Trees By
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">All</a>
+    <a class="dropdown-item" href="#">Municipality</a>
+    <a class="dropdown-item" href="#">Species</a>
   </div>
+
+</div>
  
 
     <table class="table table-hover" style="width:100%">
