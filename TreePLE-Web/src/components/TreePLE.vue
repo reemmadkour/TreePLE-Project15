13cca25641be3e1f624b9a0d3a248d19b371cc93
mahdis-asaadi <mahdis.asaadi@mail.mcgill.ca>
2018-04-07 13:21:06 -0400

    Trying to fix drop-down

--- TreePLE-Web/src/components/TreePLE.vue
+++ TreePLE-Web/src/components/TreePLE.vue
@@ -4,17 +4,22 @@
 
    
     <h2> List of all trees </h2>
+
 <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
+    List Trees By
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">All</a>
     <a class="dropdown-item" href="#">Municipality</a>
     <a class="dropdown-item" href="#">Species</a>
   </div>
-</div>
 
+</div>
+ 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
     <table class="table table-hover" style="width:100%">
 <thead>
