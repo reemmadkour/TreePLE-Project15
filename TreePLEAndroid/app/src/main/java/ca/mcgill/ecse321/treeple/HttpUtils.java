6264414e9d47ee9201a172eb05cdc34c515afa79
mahdis-asaadi <mahdis.asaadi@mail.mcgill.ca>
2018-04-09 16:10:48 -0400

    Revert "test"
    This reverts commit c4bdf401548d0efb5bba21df76171d39cb51d2ae.

--- TreePLEAndroid/app/src/main/java/ca/mcgill/ecse321/treeple/HttpUtils.java
+++ TreePLEAndroid/app/src/main/java/ca/mcgill/ecse321/treeple/HttpUtils.java
@@ -10,7 +10,7 @@ import com.loopj.android.http.RequestParams;
 
 public class HttpUtils {
 
-    //public static final String DEFAULT_BASE_URL = "http://192.168.56.50:8080/";
+    //public static final String DEFAULT_BASE_URL = "http://192.168.56.50:8088/";
     public static final String DEFAULT_BASE_URL = "http://ecse321-14.ece.mcgill.ca:8080/" ;
 
 
