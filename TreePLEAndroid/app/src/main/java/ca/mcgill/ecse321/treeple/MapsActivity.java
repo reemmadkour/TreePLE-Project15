4921fab199cd26ceae1dc029326e70305c6fe690
Lea Akkari <lea.akkari@mail.mcgill.ca>
2018-04-12 18:51:44 -0400

    List trees on map working, with status, species and location displayed on marker click

--- TreePLEAndroid/app/src/main/java/ca/mcgill/ecse321/treeple/MapsActivity.java
+++ TreePLEAndroid/app/src/main/java/ca/mcgill/ecse321/treeple/MapsActivity.java
@@ -41,6 +41,16 @@ import java.util.List;
 import cz.msebera.android.httpclient.Header;
 
 
+/**
+ * Manipulates the map once available.
+ * This callback is triggered when the map is ready to be used.
+ * This is where we can add markers or lines, add listeners or move the camera. In this case,
+ * we just add a marker near Sydney, Australia.
+ * If Google Play services is not installed on the device, the user will be prompted to install
+ * it inside the SupportMapFragment. This method will only be triggered once the user has
+ * installed Google Play services and returned to the app.
+ */
+
 public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarkerClickListener,OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -51,21 +61,16 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
 
     private String error = null;
 
-
-    private List<Double> l = new ArrayList<>();
-    private List<Double> l2 = new ArrayList<>();
-
-    List<LatLng> position = new ArrayList<>();
-
-
     private List<String> munName = new ArrayList<>();
     private ArrayAdapter<String> munAdapter;
 
     TextView msg;
 
 
-
-
+    /**
+     *
+     * @param savedInstanceState
+     */
 
 
     @Override
@@ -86,193 +91,20 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
         munAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, munName);
         munAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         munSpinner.setAdapter(munAdapter);
-
-        refreshListLat(munAdapter ,munName, "trees");
-        refreshListLgt(munAdapter ,munName, "trees");
-        position = ListTreeByLocation();
+        munSpinner.setVisibility(View.GONE);
 
 
-
-
-
+        refresh(munAdapter ,munName, "trees");
 
     }
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     *
+     * @param adapter
+     * @param names
+     * @param restFunctionName get trees
      */
-
-
-    public void refreshMap(View view) {
-        //refreshListLat(munAdapter ,munName, "trees");
-        //refreshListLgt(munAdapter ,munName, "trees");
-
-
-        int i=0;
-        i++;
-        position = ListTreeByLocation();
-        System.out.println("l" + l);
-        System.out.println("l2" + l2);
-        System.out.println("On CLICK" + position);
-
-        if(i<=1) {
-            for (LatLng l : position) {
-                Marker m = mMap.addMarker(new MarkerOptions().position(l).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)));
-                onMarkerClick(m);
-            }
-        }
-
-        mMap.setOnMarkerClickListener(this);
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        //getting latitude longitude input from plant tree page
-        Intent i = getIntent();
-        Double lat = i.getDoubleExtra("latitude", 0);
-        Double lng = i.getDoubleExtra("longitude", 0);
-
-        LatLng tree = new LatLng(lat, lng);
-
-        //creating a new marker at that position, with the tree icon
-        Marker1 = mMap.addMarker(new MarkerOptions().position(tree).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)).title("Willow"));
-
-
-
-        //on marker click, display marker position, tree status and tree species
-        onMarkerClick(Marker1);
-
-        //TODO
-
-
-       /* Intent i2 = getIntent();
-        Double lat_cut = i2.getDoubleExtra("latitude_cut", 0);
-        Double lgt_cut = i2.getDoubleExtra("longitude_cut", 0);
-
-        LatLng tree_cut = new LatLng(lat_cut, lgt_cut);
-
-        mMap.addMarker((new MarkerOptions().position(tree_cut).icon(BitmapDescriptorFactory.fromResource(R.drawable.trunk1))));*/
-
-
-
-        /*planted_markers.add(Marker1);
-
-        for(Marker marker : planted_markers){
-            mMap.addMarker((new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.tree))));
-        }*/
-
-
-
-
-
-
-
-        // ** HARDCODIND **
-
-        //Montreal: 45.5017° N, 73.5673°
-        /*LatLng tree1 = new LatLng(45.5017, -73.5673);
-        Marker2 = mMap.addMarker(new MarkerOptions().position(tree1).icon(BitmapDescriptorFactory.fromResource((R.drawable.tree))).title("Pine").snippet("Planted"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tree1));
-
-        onMarkerClick(Marker2);*/
-
-
-
-        mMap.setOnMarkerClickListener(this);
-
-    }
-
-
-
-
-    @Override
-    public boolean onMarkerClick(final Marker marker) {
-
-        if (marker.equals(marker)) {
-            //handle click here
-            Toast.makeText(this, "Position:  "  +  marker.getPosition()+ "\n"  + "Species:  " + marker.getTitle() + "\n" + "Status:  " + marker.getSnippet(),
-                    Toast.LENGTH_SHORT).show();
-            return true;
-
-        }
-        else{
-            return false;
-    }
-
-
-    }
-
-
-    private void refreshErrorMessage() {
-        // set the error message
-        TextView tvError = (TextView) findViewById(R.id.error_m);
-        tvError.setText(error);
-
-        if (error == null || error.length() == 0) {
-            tvError.setVisibility(View.GONE);
-        } else {
-            tvError.setVisibility(View.VISIBLE);
-        }
-
-    }
-
-
-    private void refreshListLat(final ArrayAdapter<String> adapter, final List<String> names,String restFunctionName) {
-        HttpUtils.get(restFunctionName, new RequestParams(), new JsonHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                names.clear();
-                names.add("Please select...");
-                for( int i = 0; i < response.length(); i++){
-                    //JSONObject mun = new JSONObject();
-                    try {
-                        //names.add(response.getJSONObject(i).getString("name"));
-                        names.add(response.getJSONObject(i).getString("latitude"));
-
-                        Double latitude = Double.parseDouble(names.get(i));
-                        if(response.getJSONObject(i).getJSONObject("currentStatus").getString("treeState").matches("Planted")) {
-                            l.add(latitude);
-                        }
-
-                        //Double.parseDouble(aString)
-
-                    } catch (Exception e) {
-                        error += e.getMessage();
-
-                    }
-                    refreshErrorMessage();
-                }
-                System.out.println(names);
-                System.out.println("list " + l);
-
-                adapter.notifyDataSetChanged();
-                //adapter2.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                try {
-                    error += errorResponse.get("message").toString();
-                } catch (JSONException e) {
-                    error += e.getMessage();
-                }
-                refreshErrorMessage();
-            }
-        });
-    }
-
-
-    private void refreshListLgt(final ArrayAdapter<String> adapter, final List<String> names,String restFunctionName) {
+    private void refresh(final ArrayAdapter<String> adapter, final List<String> names,String restFunctionName) {
         HttpUtils.get(restFunctionName, new RequestParams(), new JsonHttpResponseHandler() {
 
             @Override
@@ -288,12 +120,37 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
 
                         Double longitude = Double.parseDouble(names.get(i));
 
-                        if(response.getJSONObject(i).getJSONObject("currentStatus").getString("treeState").matches("Planted")) {
-                            l2.add(longitude);
-                        }
+                      if(response.getJSONObject(i).getJSONObject("currentStatus").getString("treeState").matches("Planted")) {
+                          Double lgt_planted = Double.parseDouble(response.getJSONObject(i).getString("longitude"));
+                          Double lat_planted = Double.parseDouble(response.getJSONObject(i).getString("latitude"));
+                          LatLng planted_p = new LatLng(lat_planted, lgt_planted);
+                          String species = response.getJSONObject(i).getString("treeSpecies");
 
 
 
+
+
+                          Marker m = mMap.addMarker(new MarkerOptions().position(planted_p).title("Planted").snippet(species).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)));
+
+                      }else if (response.getJSONObject(i).getJSONObject("currentStatus").getString("treeState").matches("ToBeCut")){
+                              Double lgt_planted = Double.parseDouble(response.getJSONObject(i).getString("longitude"));
+                              Double lat_planted = Double.parseDouble(response.getJSONObject(i).getString("latitude"));
+                              LatLng planted_p = new LatLng(lat_planted, lgt_planted);
+                              String species = response.getJSONObject(i).getString("treeSpecies");
+
+                              Marker m = mMap.addMarker(new MarkerOptions().position(planted_p).title("Marked to Be Cut").snippet(species).icon(BitmapDescriptorFactory.fromResource(R.drawable.trunk1)));
+
+
+                      }else if (response.getJSONObject(i).getJSONObject("currentStatus").getString("treeState").matches("Diseased")) {
+                          Double lgt_planted = Double.parseDouble(response.getJSONObject(i).getString("longitude"));
+                          Double lat_planted = Double.parseDouble(response.getJSONObject(i).getString("latitude"));
+                          LatLng planted_p = new LatLng(lat_planted, lgt_planted);
+                          String species = response.getJSONObject(i).getString("treeSpecies");
+
+
+
+                          Marker m = mMap.addMarker(new MarkerOptions().position(planted_p).title("Diseased").snippet(species).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree_diseased)));
+                      }
 
                         //Double.parseDouble(aString)
 
@@ -303,15 +160,10 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
                     }
                     refreshErrorMessage();
                 }
-                System.out.println(names);
-                System.out.println("list " + l2);
-
-
-
-
 
                 adapter.notifyDataSetChanged();
-                //adapter2.notifyDataSetChanged();
+
+
             }
 
             @Override
@@ -324,28 +176,62 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMarke
                 refreshErrorMessage();
             }
         });
+
     }
 
 
+    /**
+     *
+     * @param googleMap object, and setting marker click listener
+     */
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        mMap.setOnMarkerClickListener(this);
 
-
-
-
-    private List<LatLng> ListTreeByLocation(){
-        if(position.size()>0) {
-            position.clear();
-        }
-
-        for(int i=0; i<l.size(); i++){
-            LatLng p = new LatLng(l.get(i), l2.get(i));
-            position.add(p);
-        }
-
-
-        return position;
     }
+
+
+    /**
+     *
+     * @param marker
+     * @return
+     */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        if (marker.equals(marker)) {
+            //handle click here
+            Toast.makeText(this, "Position:  "  +  marker.getPosition()+ "\n"  + "Status:  " + marker.getTitle()+ "\n"  + "Species:  " + marker.getSnippet(),
+                    Toast.LENGTH_SHORT).show();
+            return true;
+
+        }
+        else{
+            return false;
+    }
+
+
+    }
+
+    /**
+     *
+     */
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error_m);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } /*else {
+            tvError.setVisibility(View.VISIBLE);
+        }*/
+
+    }
+
 
 
 
